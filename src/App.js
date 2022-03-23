3846be905a984ff4944744207ccc34c72d6ca7e4
SamuelMensah786 <bentleyk009@gmail.com>
2022-03-23 06:56:47 -0700

    React

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import writers from './writers';
 import ProfileCard from './ProfileCard';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [writers, setWriters] = useState([]);
+
+  useEffect(() => {
+    const getWirters = async () => {
+      const response = await fetch("/writers.json");
+      const data = await response.json();
+      console.log(data);
+      setWriters(data);
+    };
+    getWirters();
+  }, []);
+
   return (
     <div>
       <h1>Write Profiles</h1>
