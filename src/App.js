ca4f79d992dd1cdb906cb7c488237dc7d221831c
SamuelMensah786 <bentleyk009@gmail.com>
2022-04-05 07:19:37 -0700

    react-component-9

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Home from './pages/Home';
+import Navbar from "./components/navbar/Navbar";
+import BlogList from "./pages/BlogList";
+import BlogDetail from './pages/BlogDetail';
 
 const App = () => {
   return (
@@ -10,10 +13,14 @@ const App = () => {
     <Routes>
       <Route path="about" element={<About />} />
       <Route path="blog" element={<Blog />} />
+      <Route path="" element={<BlogList />} />
+      <Route path=":blog" element={<BlogDetail />} />
+      </Routes>
       <Route path="/" element={<Home />} />
+    <Route/>
     </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,19 +1,8 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
 
-const Blog = () => {
-  return (
-    <section>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae id, nulla dicta sunt odio ullam ipsa rem quas iste nobis error rerum, excepturi totam ratione modi suscipit cum obcaecati provident?
-        Quibusdam maxime blanditiis placeat rem iure dolores repellat a sapiente aliquid? Expedita tempora recusandae ea voluptates quidem. Ex officia exercitationem dolor vel, quasi, incidunt iste eius ducimus corporis magni architecto.
-        Placeat veritatis reprehenderit nostrum iure in dolor, nihil enim incidunt eligendi delectus voluptatum quo sapiente quod nobis explicabo deserunt. Quisquam earum odio animi impedit repellat omnis id. Necessitatibus, deserunt aliquid.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae id, nulla dicta sunt odio ullam ipsa rem quas iste nobis error rerum, excepturi totam ratione modi suscipit cum obcaecati provident?
-        Quibusdam maxime blanditiis placeat rem iure dolores repellat a sapiente aliquid? Expedita tempora recusandae ea voluptates quidem. Ex officia exercitationem dolor vel, quasi, incidunt iste eius ducimus corporis magni architecto.
-        Placeat veritatis reprehenderit nostrum iure in dolor, nihil enim incidunt eligendi delectus voluptatum quo sapiente quod nobis explicabo deserunt. Quisquam earum odio animi impedit repellat omnis id. Necessitatibus, deserunt aliquid.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae id, nulla dicta sunt odio ullam ipsa rem quas iste nobis error rerum, excepturi totam ratione modi suscipit cum obcaecati provident?
-        Quibusdam maxime blanditiis placeat rem iure dolores repellat a sapiente aliquid? Expedita tempora recusandae ea voluptates quidem. Ex officia exercitationem dolor vel, quasi, incidunt iste eius ducimus corporis magni architecto.
-        Placeat veritatis reprehenderit nostrum iure in dolor, nihil enim incidunt eligendi delectus voluptatum quo sapiente quod nobis explicabo deserunt. Quisquam earum odio animi impedit repellat omnis id. Necessitatibus, deserunt aliquid.</p>
-    </section>
-  )
+
+function Blog() {
+  return <Outlet />;
 }
 
-export default Blog
+export default Blog;
